94956b8382de072e9cee6afcd541bfa5159490e3
Vishal-Kumar <kumarfromatlanta@hotmail.com>
2015-05-21 03:45:30 -0400

    adding highlighting to our code snippet

--- config/env/all.js
+++ config/env/all.js
@@ -15,6 +15,7 @@ module.exports = {
 			css: [
 				'public/lib/bootstrap/dist/css/bootstrap.css',
 				'public/lib/bootstrap/dist/css/bootstrap-theme.css',
+				'public/lib/highlightjs/styles/agate.css'
 			],
 			js: [
 				'public/lib/angular/angular.js',
@@ -25,7 +26,9 @@ module.exports = {
 				'public/lib/angular-sanitize/angular-sanitize.js', 
 				'public/lib/angular-ui-router/release/angular-ui-router.js',
 				'public/lib/angular-ui-utils/ui-utils.js',
-				'public/lib/angular-bootstrap/ui-bootstrap-tpls.js'
+				'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
+				'public/lib/angular-highlightjs/angular-highlightjs.min.js',
+				'public/lib/highlightjs/highlight.pack.js'
 			]
 		},
 		css: [
--- public/config.js
+++ public/config.js
@@ -4,7 +4,7 @@
 var ApplicationConfiguration = (function() {
 	// Init module configuration options
 	var applicationModuleName = 'code-snippets';
-	var applicationModuleVendorDependencies = ['ngResource', 'ngCookies',  'ngAnimate',  'ngTouch',  'ngSanitize',  'ui.router', 'ui.bootstrap', 'ui.utils'];
+	var applicationModuleVendorDependencies = ['ngResource', 'ngCookies',  'ngAnimate',  'ngTouch',  'ngSanitize',  'ui.router', 'ui.bootstrap', 'ui.utils', 'hljs'];
 
 	// Add a new vertical module
 	var registerModule = function(moduleName, dependencies) {
